c51df998b395c80af4c401fc2a1304cfed5ebc71
Joel <joel.cidmouzinho@gmx.net>
2024-09-16 11:04:16 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/;
+    return passwordRegex.test(password);
+}
+
+module.exports = {
+    reverseString,
+    validateEmail,
+    validatePassword
+};
